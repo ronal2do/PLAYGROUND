29f6a5747a29c222af833e3d2321fdba20f0025a
Ronaldo <ronal2do@gmail.com>
2017-04-25 18:59:07 -0300

    menu

--- xulapallax/src/App.js
+++ xulapallax/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import About from './About';
 import Work from './Work';
+import flags from './images/flags.png';
+import './App.css';
 
 class App extends Component {
     render() {
         return (
           <div>
+            <div className="nav">
+              <div className="container">
+                <img src={flags} alt="" className="flags"/>
+              </div>
+            </div>
             <About />
             <Work />
           </div>
